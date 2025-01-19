5087c28b3c693d99db3292a3d80d2e24d89d673e
Manav Patil <patilmanav22@gmail.com>
2025-01-19 09:18:32 +0530

    Token Added

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,12 @@
 const LocalConfig = {
   values: {
-    NEXT_PUBLIC_RESUME_LINK: 'https://my-portfolio-n82i.onrender.com/resume/MyResume.pdf',
+    TZ: process.env.TZ,
+    NODE_ENV: process.env.NODE_ENV,
+    AUTOPREFIXER_GRID: process.env.AUTOPREFIXER_GRID,
+    NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID,
+    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+    NEXT_PUBLIC_VERCEL_TOKEN: process.env.NEXT_PUBLIC_VERCEL_TOKEN,
+    NEXT_PUBLIC_RESUME_LINK: "https://my-portfolio-n82i.onrender.com/resume/MyResume.pdf",
   },
 };
 
